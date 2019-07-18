cdbbf1252110c9ac2fd5da7c090570946594a58f
Dan Markham <dmarkham@gmail.com>
2019-07-18 08:06:39 -0700

    Port fix from @ltpquang

--- enumer.go
+++ enumer.go
@@ -76,7 +76,7 @@ func (g *Generator) buildBasicExtras(runs [][]Value, typeName string, runsThresh
 	g.Printf(stringNameToValueMethod, typeName)
 	g.Printf(stringValuesMethod, typeName)
 	g.Printf(stringsMethod, typeName)
-	if len(runs) < runsThreshold {
+	if len(runs) <= runsThreshold {
 		g.Printf(stringBelongsMethodLoop, typeName)
 	} else { // There is a map of values, the code is simpler then
 		g.Printf(stringBelongsMethodSet, typeName)
