2cd1d1c7c3550a4b2a8be610c70fa5748be58b01
igormiku <>
2022-03-17 19:43:50 +0200

    add posibility to reference as separate module

--- examples/gomods/main.go
+++ examples/gomods/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-//go:generate go run github.com/dmarkham/enumer -type=Pill -json
+//go:generate go run github.com/igormiku/enumer -type=Pill -json
 type Pill int
 
 const (
